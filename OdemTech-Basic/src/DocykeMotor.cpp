419ec5bf46f99a6ab15637bc66abdab8dfa1115e
Nisgush <151662214+Nisgush@users.noreply.github.com>
2025-03-26 12:10:00 +0200

    Update DocykeMotor.cpp
    Added dual motor method

--- OdemTech-Basic/src/DocykeMotor.cpp
+++ OdemTech-Basic/src/DocykeMotor.cpp
@@ -68,3 +68,82 @@ void DocykeMotor::drive(int speed, int time){
 void DocykeMotor::drive(int speed){
     drive(speed, 10);
 }
+
+uint8_t DocykeMotor::getVelPin(){
+    return velocityPin;
+}
+
+/*
+void dualDrive(DocykeMotor motor1, DocykeMotor motor2, int speed1, int speed2){
+    if(speed1>255){
+        speed1 = 255;
+    } else if (speed1<-255){
+        speed1 = -255;
+    }
+    if(speed1>=0 && !motor1.getDirection()){
+        motor1.setDirection(false);
+    }
+    if(speed1>=0){
+        analogWrite(motor1.getVelPin(),speed1);
+    }
+    if(speed1<0 && motor1.getDirection()){
+        motor1.setDirection(true);
+    }
+    if(speed1<0){
+        analogWrite(motor1.getVelPin(),-speed1);
+    }
+    if(speed2>255){
+        speed2 = 255;
+    } else if (speed2<-255){
+        speed2 = -255;
+    }
+    if(speed2>=0 && !motor2.getDirection()){
+        motor2.setDirection(false);
+    }
+    if(speed2>=0){
+        analogWrite(motor2.getVelPin(),speed2);
+    }
+    if(speed2<0 && motor2.getDirection()){
+        motor2.setDirection(true);
+    }
+    if(speed2<0){
+        analogWrite(motor2.getVelPin(),-speed2);
+    }
+    delay(150);
+    analogWrite(motor1.getVelPin(),0);
+    analogWrite(motor2.getVelPin(),0);
+}
+    
+*/
+   
+   void dualDrive(DocykeMotor motor1, DocykeMotor motor2, int speed1, int speed2) {
+        // Clamp speed1 between -255 and 255
+        speed1 = constrain(speed1, -255, 255);
+        speed2 = constrain(speed2, -255, 255);
+    
+        // Set direction and speed for motor1
+        if (speed1 < 0) {
+            motor1.setDirection(true);  // Reverse
+            analogWrite(motor1.getVelPin(), -speed1);  // Write positive PWM
+        } else {
+            motor1.setDirection(false);  // Forward
+            analogWrite(motor1.getVelPin(), speed1);
+        }
+    
+        // Set direction and speed for motor2
+        if (speed2 < 0) {
+            motor2.setDirection(true);  // Reverse
+            analogWrite(motor2.getVelPin(), -speed2);  // Write positive PWM
+        } else {
+            motor2.setDirection(false);  // Forward
+            analogWrite(motor2.getVelPin(), speed2);
+        }
+    
+        // Run motors for a short time
+        delay(150);
+    
+        // Stop motors after delay
+        analogWrite(motor1.getVelPin(), 0);
+        analogWrite(motor2.getVelPin(), 0);
+    }
+        
